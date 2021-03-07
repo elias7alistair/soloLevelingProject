ab23221a30b13e0f6d80186bb362c8ec8d22743b
AlistairFernandes <alistair.fernandes@fyntune.com>
2021-03-07 20:19:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Particles from "react-particles-js";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Particles
+        className='particles'
+        width='100vw'
+        height='100vh'
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 500,
+              },
+            },
+          },
+        }}
+      />
+      <h1>Solo Leveling</h1>
     </div>
   );
 }
