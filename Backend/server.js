f6e36f41c2c0abc22dde463c96d825ffc6827d2a
elias7alistair <falistair7@gmail.com>
2021-10-04 13:47:52 +0530

    gdsa

--- Backend/server.js
+++ Backend/server.js
@@ -19,10 +19,10 @@ app.use("/api/quests", questRoutes);
 app.use("/api/users", userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '/Frontend/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'Frontend', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
